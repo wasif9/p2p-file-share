80941b58c68f9da53ef83d672c9202a76dd6b409
tim-macphail <timothy.macphail@gmail.com>
2025-02-26 17:59:00 -0700

    hello world http server
    Signed-off-by: tim-macphail <timothy.macphail@gmail.com>

--- db-manager/main.go
+++ db-manager/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "You requested: %s\n", r.URL.Path)
+}
 
 func main() {
-	fmt.Println("hello world")
+	// prefix-based pattern matching means any route with
+	// the / prefix (all of them) will be served by function 'handler'
+	http.HandleFunc("/", handler)
+
+	fmt.Println("Server starting on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
