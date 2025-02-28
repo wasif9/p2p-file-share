28531eacf17a73fcb1d92c3e4753c32c41657813
tim-macphail <timothy.macphail@gmail.com>
2025-02-28 14:14:45 -0700

    simple connection to database
    Signed-off-by: tim-macphail <timothy.macphail@gmail.com>

--- db-manager/main.go
+++ db-manager/main.go
@@ -3,20 +3,37 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "You requested: %s\n", r.URL.Path)
 }
 
+const PORT = "8080"
+
 func main() {
 	// prefix-based pattern matching means any route with
 	// the / prefix (all of them) will be served by function 'handler'
 	http.HandleFunc("/", handler)
 
-	fmt.Println("Server starting on port 8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	// TODO: read this from config
+	index := 0 // the index of this DB instance
+
+	dsn := fmt.Sprintf("host=localhost user=postgres password=password dbname=registry%d port=5432 sslmode=disable TimeZone=UTC", index)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v\n\n", db.Config)
+
+	fmt.Printf("Server starting on port %s...\n", PORT)
+	if err := http.ListenAndServe(net.JoinHostPort("localhost", PORT), nil); err != nil {
 		log.Fatal(err)
 	}
 }
--- db-manager/readme.md
+++ db-manager/readme.md
@@ -14,3 +14,17 @@ curl localhost:8080/foo
 ```
 
 Or just go to http://localhost:8080/whatever 🙂
+
+# Setting up DB
+
+## Install posgres 
+https://www.postgresql.org/download/
+
+## Creating the registry[x] database
+
+```bash
+psql -u postgres
+```
+
+```sql
+CREATE DATABASE registry0;
