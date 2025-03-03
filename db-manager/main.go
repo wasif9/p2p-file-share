215e628a13c2442925ca09a0d5c21f1079d46797
tim-macphail <timothy.macphail@gmail.com>
2025-03-03 14:56:14 -0700

    create handler function skeletons for crd operations
    Signed-off-by: tim-macphail <timothy.macphail@gmail.com>

--- db-manager/main.go
+++ db-manager/main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"net/url"
+	"strings"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -17,8 +19,36 @@ type Record struct {
 	Name string
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "You requested: %s\n", r.URL.Path)
+func handlePostRecord(w http.ResponseWriter, r *http.Request) {
+	var response string = "handling POST\n"
+
+	w.WriteHeader(http.StatusCreated)
+	// write the response
+	fmt.Fprintln(w, response)
+}
+func handleGetRecord(w http.ResponseWriter, r *http.Request) {
+	s := strings.Split(r.URL.Path, "/")
+
+	var response string = "handling GET " + s[len(s)-1]
+
+	fmt.Fprintln(w, response)
+
+}
+func handleDeleteRecord(w http.ResponseWriter, r *http.Request) {
+	var response string = "handling DELETE\n"
+	fmt.Fprintln(w, response)
+
+}
+
+// Handles http requests to the route '/records'
+func recordHandler(w http.ResponseWriter, r *http.Request) {
+	handlers := map[string]func(http.ResponseWriter, *http.Request){
+		http.MethodPost:   handlePostRecord,
+		http.MethodGet:    handleGetRecord,
+		http.MethodDelete: handleDeleteRecord,
+	}
+
+	handlers[r.Method](w, r)
 }
 
 /***********************
@@ -58,21 +88,24 @@ func deleteRecord(db *gorm.DB, id uint) {
 
 }
 
+// Server Instance Configuration:
+// TODO: read this from a config.json or similar
+const index = 0 // the index of this DB instance
 const PORT = "8080"
+const VERSION = "v1"
 
 func main() {
-	var err error
+	var err error // re-useable error
 
-	// prefix-based pattern matching means any route with
-	// the / prefix (all of them) will be served by function 'handler'
-	http.HandleFunc("/", handler)
+	basePath, err := url.JoinPath("/", "api", VERSION, "records", "/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	http.HandleFunc(basePath, recordHandler)
 
-	// TODO: read this from config
-	index := 0 // the index of this DB instance
-
+	// Connection to the database
 	dsn := fmt.Sprintf("host=localhost user=postgres password=password dbname=registry%d port=5432 sslmode=disable TimeZone=UTC", index)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
 	if err != nil {
 		log.Fatal(err)
 	}
