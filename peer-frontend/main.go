90317472e5f04b09f03d952f423f5c70261c5793
Chenghou Si <chenghou.si@ucalgary.ca>
2025-03-04 01:54:32 -0700

    Main and Download UI using GioUI

--- peer-frontend/main.go
+++ peer-frontend/main.go
@@ -1,29 +1,111 @@
 package main
 
 import (
-	"fmt"
-	"os"
+    "image/color"
+    "log"
 
-	qt "github.com/mappu/miqt/qt6"
+    "gioui.org/app"
+    "gioui.org/layout"
+	"gioui.org/unit"
+	"gioui.org/op"
+    "gioui.org/widget"
+    "gioui.org/widget/material"
 )
 
+const (
+	DownloadTab = iota		// 0
+	UploadTab				// 1
+	ProgressTab				// 2
+)
+
+var tabSelected = DownloadTab
+
 func main() {
+    go func() {
+        w := new(app.Window)
+		w.Option(app.Title("P2P File Share"))
+		w.Option(app.Size(unit.Dp(800), unit.Dp(600)))
 
-	qt.NewQApplication(os.Args)
+        th := material.NewTheme()
+		var ops op.Ops
+		
+		tabButtons := make([]widget.Clickable, 3)
 
-	btn := qt.NewQPushButton3("Hello world!")
-	btn.SetFixedWidth(320)
+		// DownloadUI instance
+		dnUI := &DownloadUI{
+			list: widget.List{
+				List: layout.List{
+					Axis: layout.Vertical,
+				},
+			},
+		}
 
-	var counter int = 0
+		// Event
+		for {
+			e := w.Event()
+			switch e := e.(type) {
+			case app.DestroyEvent:
+				log.Fatal(e.Err)
+			case app.FrameEvent:
+				gtx := app.NewContext(&ops, e)
 
-	btn.OnPressed(func() {
-		counter++
-		btn.SetText(fmt.Sprintf("You have clicked the button %d time(s)", counter))
+				// Layout the tabs
+				layout.Flex{Axis: layout.Horizontal}.Layout(gtx,
+					// Tabs on the left
+					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+						return layout.Flex{Axis: layout.Vertical}.Layout(gtx,
+							tab_Btn(th, gtx, &tabButtons[0], "Download", DownloadTab),
+							tab_Btn(th, gtx, &tabButtons[1], "Upload", UploadTab),
+							tab_Btn(th, gtx, &tabButtons[2], "Progress", ProgressTab),
+						)
+					}),
+
+					// Right content of the selected tab
+					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
+						if tabSelected == DownloadTab {
+							return dnUI.DownloadLayout(gtx, th)
+						} else if tabSelected == UploadTab {
+							return material.Body1(th, "Upload").Layout(gtx)
+						} else {
+							return material.Body1(th, "Download Progress").Layout(gtx)
+						}
+					}),
+					
+				)
+				e.Frame(gtx.Ops)
+			}
+		}
+    }()
+    app.Main()
+}
+
+func tab_Btn(th *material.Theme, gtx layout.Context, button *widget.Clickable, title string, tab int) layout.FlexChild {
+	return layout.Flexed(0.7, func(gtx layout.Context) layout.Dimensions {
+		// Padding
+		inset := layout.Inset{
+			Top:    10,
+			Right:  20,
+			Bottom: 10,
+			Left:   20,
+		}
+
+		// Apply the padding and layout the button
+		return inset.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+			btn := material.Button(th, button, title)
+
+			// Button property
+			gtx.Constraints.Min.X = gtx.Dp(120)
+			gtx.Constraints.Min.Y = gtx.Dp(80)
+			btn.TextSize = unit.Sp(20)
+			btn.Color = color.NRGBA{R: 0, G: 0, B: 0, A: 255}
+			btn.Background = color.NRGBA{R: 255, G: 255, B: 255, A: 255}
+	
+			// Click event
+			if (*button).Clicked(gtx) {
+				tabSelected = tab
+			}
+	
+			return btn.Layout(gtx)
+		})
 	})
-
-	btn.Show()
-
-	qt.QApplication_Exec()
-
-	fmt.Println("OK!")
 }
