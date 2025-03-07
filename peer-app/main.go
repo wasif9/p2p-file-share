bddb8222f4ff3ab119bc06f9845ffefde89b4736
tim-macphail <timothy.macphail@gmail.com>
2025-03-07 11:04:38 -0700

    combine main()s
    Signed-off-by: tim-macphail <timothy.macphail@gmail.com>

--- peer-app/main.go
+++ peer-app/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 	"log"
+	"os"
+	"time"
 
 	"gioui.org/app"
 	"gioui.org/layout"
@@ -10,6 +13,13 @@ import (
 	"gioui.org/unit"
 	"gioui.org/widget"
 	"gioui.org/widget/material"
+	libp2p "github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/multiformats/go-multiaddr"
+	"github.com/wasif9/p2p-file-share/discovery"
+	"github.com/wasif9/p2p-file-share/messaging"
+	"github.com/wasif9/p2p-file-share/network"
+	"github.com/wasif9/p2p-file-share/transfer"
 )
 
 const (
@@ -26,6 +36,51 @@ var tabSelected = DownloadTab
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	// Create a new libp2p node (peer)
+	node, err := libp2p.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Print the node's multiaddresses
+	fmt.Println("Node ID:", node.ID().String())
+	for _, addr := range node.Addrs() {
+		fmt.Println("Listening on:", addr)
+	}
+
+	// Enable DHT for file lookup
+	// dht.SetupDHT(node)  // Commented out for now
+
+	// Enable mDNS-based peer discovery
+	discovery.SetupDiscovery(node)
+
+	// Handle incoming messages
+	messaging.HandleMessages(node)
+
+	// Handle incoming file requests
+	transfer.HandleFileRequests(node)
+
+	if len(os.Args) > 3 && os.Args[1] == "send-file" {
+		targetAddr := os.Args[2]
+		filePath := os.Args[3]
+
+		// Connect to the target peer
+		network.ConnectToPeer(node, targetAddr)
+
+		// ⬇️ Add this delay to ensure connection is fully established
+		time.Sleep(1 * time.Second)
+
+		// Extract peer ID from multiaddr
+		addr, _ := multiaddr.NewMultiaddr(targetAddr)
+		peerInfo, _ := peer.AddrInfoFromP2pAddr(addr)
+		fmt.Println("Attempting to send file to peer")
+
+		// Send a file to the target peer
+		transfer.SendFile(node, peerInfo.ID, filePath)
+	}
+
+	// Keep the node running
 	go func() {
 		w := new(app.Window)
 		w.Option(app.Title("P2P File Share"))
