cec9f2e919d67ad4a24d24078a3b9a6a85f45cfa
tim-macphail <timothy.macphail@gmail.com>
2025-03-11 14:54:22 -0600

    remove done todos
    Signed-off-by: tim-macphail <timothy.macphail@gmail.com>

--- pkg/models/types.go
+++ pkg/models/types.go
@@ -2,17 +2,11 @@ package types
 
 import "time"
 
-// todo: move this to a models.go file/package
-/****************************************
-* Struct for the database records
-*
-* **************************************/
 type Manifest struct {
 	Name string `gorm:"primaryKey" json:"name"`
 	Hash string `json:"hash"`
 }
 
-// TODO: move this to common package so that client can use too
 type Heartbeat struct {
 	Index       int           `json:"node-index"`
 	Uptime      time.Duration `json:"uptime"`
