1481bdb1e2f83879b835fb63f1194f251020ec6e
Tim Macphail <timothy.macphail@gmail.com>
2025-03-28 12:07:58 -0600

    func to check local ip addr

--- db-manager/configuration.go
+++ db-manager/configuration.go
@@ -3,11 +3,14 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
+	"net"
 	"os"
 )
 
 type Configuration struct {
+	Host        string `json:"host"`
 	Index       int    `json:"index"`
 	Port        string `json:"port"`
 	Version     string `json:"version"`
@@ -21,6 +24,8 @@ type Configuration struct {
 var cfg Configuration
 
 func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	if len(os.Args) < 2 {
 		log.Fatal("config file not provided")
 	}
@@ -31,12 +36,63 @@ func init() {
 	if err != nil {
 		log.Fatal(errors.Join(errors.New("Failed to read server configuration file"), err))
 	}
-	_ = bytes
 
-	err = json.Unmarshal(bytes, &cfg)
+	var cfgs []Configuration
+
+	err = json.Unmarshal(bytes, &cfgs)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("Loaded configuration %+v from %s\n", cfg, configFilePath)
+	for _, config := range cfgs {
+		if isLocalIP(config.Host) {
+			cfg = config
+		}
+	}
+
+	log.Printf("Loaded configurations %+v from %s\n", cfgs, configFilePath)
+}
+
+func isLocalIP(ipStr string) bool {
+	// Parse the input IP address
+	inputIP := net.ParseIP(ipStr)
+	if inputIP == nil {
+		fmt.Println("Invalid IP address")
+		return false
+	}
+
+	// Get all network interfaces
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Println("Error getting network interfaces:", err)
+		return false
+	}
+
+	// Iterate through network interfaces
+	for _, iface := range interfaces {
+		// Get addresses for this interface
+		addrs, err := iface.Addrs()
+		if err != nil {
+			continue
+		}
+
+		// Check each address
+		for _, addr := range addrs {
+			// Convert to IP network
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+
+			// Compare IPs
+			if ip != nil && ip.Equal(inputIP) {
+				return true
+			}
+		}
+	}
+
+	return false
 }
