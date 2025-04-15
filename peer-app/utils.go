1092bf45ab43099355275f57a7f25307354f4280
wasif9 <wasifuddowlah@gmail.com>
2025-04-14 19:07:32 -0600

    Implemented chunking in upload and added manifest file implementation

--- peer-app/utils.go
+++ peer-app/utils.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/ipfs/go-cid"
 	mh "github.com/multiformats/go-multihash"
@@ -43,4 +44,59 @@ func cidFromFile(filepath string) (cid.Cid, error) {
 	}
 
 	return cid.NewCidV1(cid.Raw, mhBytes), nil
+}
+func cidFromBytes(data []byte) (cid.Cid, error) {
+	h := sha256.Sum256(data)
+	mhBytes, err := mh.Encode(h[:], mh.SHA2_256)
+	if err != nil {
+		return cid.Cid{}, err
+	}
+	return cid.NewCidV1(cid.Raw, mhBytes), nil
+}
+
+func chunkAndStoreChunks(filePath string) (map[int]string, cid.Cid, error) {
+	const chunkSize = 512 * 1024
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, cid.Cid{}, err
+	}
+	defer file.Close()
+
+	chunkMap := make(map[int]string)
+	chunkFolder := filepath.Join(".p2p", filepath.Base(filePath)+"_chunks")
+	os.MkdirAll(chunkFolder, os.ModePerm)
+
+	var fullData []byte
+	buf := make([]byte, chunkSize)
+	chunkIndex := 0
+
+	for {
+		n, err := file.Read(buf)
+		if n > 0 {
+			chunk := buf[:n]
+			fullData = append(fullData, chunk...)
+			chunkCID, err := cidFromBytes(chunk)
+			if err != nil {
+				return nil, cid.Cid{}, err
+			}
+
+			chunkFilePath := filepath.Join(chunkFolder, chunkCID.String())
+			if err := os.WriteFile(chunkFilePath, chunk, 0644); err != nil {
+				return nil, cid.Cid{}, err
+			}
+			chunkMap[chunkIndex] = chunkCID.String()
+			chunkIndex++
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, cid.Cid{}, err
+		}
+	}
+
+	fullHash := sha256.Sum256(fullData)
+	mhBytes, _ := mh.Encode(fullHash[:], mh.SHA2_256)
+	fullFileCID := cid.NewCidV1(cid.Raw, mhBytes)
+	return chunkMap, fullFileCID, nil
 }
